d85a41667f0beb07e5a5f97579985e7d8fa93608
awesomejang <uljang1995@gmail.com>
2022-04-14 00:04:48 +0900

    학급회장 수정

--- src/main/java/inflearn/hash/학급회장.java
+++ src/main/java/inflearn/hash/학급회장.java
@@ -34,11 +34,18 @@ public class 학급회장 {
 		return answer;
 	}
 	
-	public static String solution(int n, String s) {
+	public static char solution2(int n, String s) {
 		char answer = ' '; //공백은된다.
 		Map<Character, Integer> map = new HashMap<Character, Integer>();
 		for(char x : s.toCharArray()) {
-			map.put(x, map.getOrDefault(map, null));
+			map.put(x, map.getOrDefault(x, 0) +1); //getOrDefault() 첫번째 파라미터의 key로 value가 있으면 리턴하고 없다면 두번째 입력된 디펄트value가 리턴된다.
+		}
+		int max = Integer.MIN_VALUE;
+		for(char key : map.keySet()) { // set리턴인데 여기서는 사용가능 
+			if(map.get(key) > max) {
+				max = map.get(key);
+				answer = key;
+			}
 		}
 		return answer;
 	}
