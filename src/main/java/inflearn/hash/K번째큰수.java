80c3c072d1dc997b3b32cb2f0fd4bf1f8ea9b3a0
awesomejang <uljang1995@gmail.com>
2022-04-18 23:33:51 +0900

    K번째 큰수

--- src/main/java/inflearn/hash/K번째큰수.java
+++ src/main/java/inflearn/hash/K번째큰수.java
@@ -1,5 +1,31 @@
 package inflearn.hash;
 
-public class K번째큰수 {
+import java.util.Collections;
+import java.util.TreeSet;
 
+public class K번째큰수 {
+	public int solution(int[] arr, int n, int k) {
+		int answer = -1;
+		TreeSet<Integer> Tset = new TreeSet<>(Collections.reverseOrder()); //내림차순
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; j++) {
+				for (int l = j + 1; l < n; l++) {
+					Tset.add(arr[i] + arr[j] + arr[l]);
+				}
+			}
+		}
+		int cnt = 0;
+		// Tset.remove(143);
+		// System.out.println(Tset.size());
+		// System.out.println("first : "+ Tset.first());
+		// System.out.println("last : "+ Tset.last());
+
+		for (int x : Tset) {
+			// System.out.println(x);
+			cnt++;
+			if (cnt == k)
+				return x;
+		}
+		return answer;
+	}
 }
